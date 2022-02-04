47d45bc906e42484bdb9f5bd950742a096f53c13
kvv1968 <kvv1968@mail.ru>
2022-02-04 18:56:01 +0300

    commit #2

--- src/test/java/ru/platform/learning/databasehandler/DatabaseHandlerApplicationTests.java
+++ src/test/java/ru/platform/learning/databasehandler/DatabaseHandlerApplicationTests.java
@@ -1,13 +1,13 @@
-package ru.platform.learning.databasehandler;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DatabaseHandlerApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package ru.platform.learning.databasehandler;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DatabaseHandlerApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
